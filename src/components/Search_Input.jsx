a1b95e21069e7a2f7c96121d08d994ead556c1b0
shishirdubey24 <shishirdubey221@gmail.com>
2025-04-02 12:04:53 +0530

    Updated GEmini Import

--- src/components/Search_Input.jsx
+++ src/components/Search_Input.jsx
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from "react";
-import { AIchatSession } from "../Config/Gemini-Ai";
+import { AIchatSession } from "../Config/GeminiAI";
 import { useLocation } from "react-router-dom";
 import User from "./UserSearch";
 const Search = () => {
