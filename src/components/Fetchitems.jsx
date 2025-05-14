9dab25b431693e6bcbed8ba041e3eafb1d6325f0
shishirdubey24 <shishirdubey221@gmail.com>
2025-05-14 14:50:43 +0530

    Commit

--- src/components/Fetchitems.jsx
+++ src/components/Fetchitems.jsx
@@ -10,7 +10,7 @@ const FetchItems = () => {
   
     const fetchData=async()=>{
      try {
-       await new Promise(resolve => setTimeout(resolve, 1000));
+       await new Promise(resolve => setTimeout(resolve, 500));
       const res= await  fetch("/items.json" )
        const data= await res.json();
        return data;
