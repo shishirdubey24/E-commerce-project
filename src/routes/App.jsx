06392a752ef2d2a2e0a1f6cc939d8cd8ddc07288
shishirdubey24 <shishirdubey221@gmail.com>
2025-07-31 18:44:56 +0530

    Modified

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,20 +1,19 @@
 import { Outlet } from "react-router";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-//import FetchItems from "../components/Fetchitems";
-//import MenProduct from "../components/MenProductFetch";
+import Footer from "../components/UI/Footer"
+import Header from "../components/pages/HomePage/Header";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HomeUI from "../components/UI/HomeUI";
-import MenUI from "../components/UI/MenUI";
+import {HomeDataMap} from "../components/hooks/HomeDataMap";
+import {NavbarFetch} from "../components/hooks/NavbarFetch";
 function App() {
    console.log(import.meta.env.VITE_APPWRITE_URL)
   return (
     <>
      <Header/>
      
-      <HomeUI/>
+      <HomeDataMap/>
       
-       <MenUI/>
+      <NavbarFetch/>
        <main className="main-content">
         <Outlet /> {/* Dynamically loads route content, e.g., Home */}
       </main>
