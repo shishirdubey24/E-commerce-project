f8332ed37d8f227a2fe04a13a5c42fae37736938
shishirdubey24 <shishirdubey221@gmail.com>
2025-07-31 18:46:18 +0530

    Chnaged

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,16 +1,15 @@
 
 import { useSelector } from "react-redux";
-import Homeitem from "../components/UI/Homeitem";
+import Homeitem from "../components/pages/HomePage/Homeitem";
 
 const Home = () => {
     const items = useSelector((store) => store.items);
-     const bagitems = useSelector(store => store.bag) || []; // Fallback to an empty array
- // const elementFound = bagitems.indexOf(item.id) >= 0;
+     const bagitems = useSelector(store => store.bag) || []; 
     return (
        <main>
   <div className="items-container">
     {items.map((item) => {
-      const isInBag = bagitems.includes(item.id); // or whatever logic
+      const isInBag = bagitems.includes(item.id); 
       return <Homeitem key={item.id} item={item} isInBag={isInBag} />;
     })}
   </div>
