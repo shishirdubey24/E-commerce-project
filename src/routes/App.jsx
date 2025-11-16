fea1f7ed78100a426fb50dcdb4a878da48716be1
shishirdubey24 <shishirdubey221@gmail.com>
2025-11-16 18:54:21 +0530

    Added HeroSection import

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 import Footer from "../components/UI/Footer"
 import Header from "../components/pages/HomePage/Header";
-
+import HeroSection from "../components/pages/HomePage/HeroSection";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {HomeDataMap} from "../components/hooks/HomeDataMap";
 import {NavbarFetch} from "../components/hooks/NavbarFetch";
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
      <Header/>
-     
+       <HeroSection/>
       <HomeDataMap/>
       
       <NavbarFetch/>
