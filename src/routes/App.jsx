0617b60f315e42560ccf0d2a0bd3c0d409d2994f
shishirdubey24 <shishirdubey221@gmail.com>
2025-11-18 12:01:08 +0530

    Changed the layout

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,16 +1,13 @@
 import { Outlet } from "react-router";
 import Footer from "../components/UI/Footer"
 import Header from "../components/pages/HomePage/Header";
-import HeroSection from "../components/pages/HomePage/HeroSection";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {HomeDataMap} from "../components/hooks/HomeDataMap";
 import {NavbarFetch} from "../components/hooks/NavbarFetch";
 function App() {
   return (
     <>
      <Header/>
-       <HeroSection/>
-      <HomeDataMap/>
+     
       
       <NavbarFetch/>
        <main className="main-content">
