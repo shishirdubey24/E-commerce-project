40a1de01468d6dc45d4b68243dd4bbf40e3965c7
shishirdubey24 <shishirdubey221@gmail.com>
2025-11-18 19:10:40 +0530

    Commit

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,14 +2,12 @@ import { Outlet } from "react-router";
 import Footer from "../components/UI/Footer"
 import Header from "../components/pages/HomePage/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {NavbarFetch} from "../components/hooks/NavbarFetch";
+//import {NavbarFetch} from "../components/hooks/NavbarFetch";
 function App() {
   return (
     <>
      <Header/>
-     
-      
-      <NavbarFetch/>
+     {/* <NavbarFetch/>*/} 
        <main className="main-content">
         <Outlet /> {/* Dynamically loads route content, e.g., Home */}
       </main>
