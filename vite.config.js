557e00b55a5221a3bd5f6fdef0e03e144bce3838
shishirdubey24 <shishirdubey221@gmail.com>
2025-11-18 12:03:49 +0530

    Added tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
-
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer({ open: true })],
+  plugins: [react(), visualizer({ open: true }), tailwindcss()],
 });
