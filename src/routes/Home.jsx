32d907a56944c997c4451877e6b0355bbee0f800
shishirdubey24 <shishirdubey221@gmail.com>
2025-12-06 21:27:51 +0530

    Changed the flow of fome page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,14 +1,12 @@
-// routes/Home.jsx - updated grid to show 4 products per row (responsive)
 import { useSelector } from "react-redux";
-import Homeitem from "../components/pages/HomePage/Homeitem";
 import { HomeDataMap } from "../components/hooks/HomeDataMap";
 import HeroSection from "../components/pages/HomePage/HeroSection";
 import CategorySection from "../components/pages/HomePage/CategorySection";
 import BrandSection from "../components/pages/HomePage/BrandSection";
-
+import FeaturedSection from "../components/pages/HomePage/FeaturedSection";
 const Home = () => {
   const items = useSelector((store) => store.items || []);
-  const bagItems = useSelector((store) => store.bag || []);
+ 
 
   return (
     <main>
@@ -17,23 +15,8 @@ const Home = () => {
       <CategorySection />
 
       <HomeDataMap />
-
-      {/* Products grid: 1 / 2 / 3 / 4 columns (mobile -> desktop) */}
-      <section className="w-[95%] mx-auto my-12">
-        {items.length > 0 ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {items.map((item) => {
-              const isInBag = bagItems.some((bagItem) => bagItem.id === item.id);
-              return <Homeitem key={item.id} item={item} isInBag={isInBag} />;
-            })}
-          </div>
-        ) : (
-          <div className="w-full py-20 flex flex-col items-center justify-center text-center text-gray-600">
-            <h3 className="text-xl font-medium mb-2">No products available</h3>
-            <p>Loading from server...</p>
-          </div>
-        )}
-      </section>
+            <FeaturedSection items={items} />
+     
     </main>
   );
 };
