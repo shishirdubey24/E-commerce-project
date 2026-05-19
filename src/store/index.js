d0a01d4091069090d31706a64e97b8cedda4f6d4
Shishir Dubey <shishirdubey221@gmail.com>
2026-05-19 21:52:05 +0530

    Updated reducer list

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,6 @@ import itemsSlice from "./itemsSlice";
 import bagReducer from "./bagSlice";
 import MendataSlice from "./MendataSlice";
 import authSlice from "./authSlice";
-import categorySlice from "./categorySlice";
 import adminPanelSlice from "./adminSlice";
 const myntraStore = configureStore({
   reducer: {
@@ -12,7 +11,6 @@ const myntraStore = configureStore({
     bag: bagReducer,
     mendata: MendataSlice.reducer,
     auth: authSlice.reducer,
-    category: categorySlice.reducer,
     adminPanel: adminPanelSlice.reducer,
   },
 });
