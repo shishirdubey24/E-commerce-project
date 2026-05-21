2e871f149277890138fa2b87ca5e32e65c423ca8
Shishir Dubey <shishirdubey221@gmail.com>
2026-05-21 23:08:12 +0530

    Added SignUp routing

--- actual-backend/Router/AuthRouter.js
+++ actual-backend/Router/AuthRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { SignupValidation } from "../Middleware/AuthValidation/SignupValidation.js";
 // import { SignInValidation } from "../Middleware/AuthValidation/SignInValidation.js"; // Uncomment when ready
-
+import { SignUp } from "../Controller/Auth/SignUp.js";
 const router = express.Router(); //  This is correct
 
 //  Register signup route
-router.post("/signup", SignupValidation);
+router.post("/signup", SignupValidation, SignUp);
 
 //  Register signin route (when implemented)
 // router.post("/signin", SignInValidation);
