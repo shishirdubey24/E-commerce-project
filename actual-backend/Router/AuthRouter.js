7a37cbf745373cfd616a8195a67e7fe6a8d54012
Shishir Dubey <shishirdubey221@gmail.com>
2026-05-28 22:29:11 +0530

    Upated router added Jwt route

--- actual-backend/Router/AuthRouter.js
+++ actual-backend/Router/AuthRouter.js
@@ -3,11 +3,14 @@ import { SignupValidation } from "../Middleware/AuthValidation/SignupValidation.
 import { SignInValidation } from "../Middleware/AuthValidation/SignInValidation.js"; // Uncomment when ready
 import { SignUp } from "../Controller/Auth/SignUp.js";
 import { signIn } from "../Controller/Auth/SignIn.js";
+import { JwtValiadtion } from "../Middleware/AuthValidation/JwtValidation.js";
+import { JwtLogin } from "./../Controller/Auth/JwtLogin.js";
 const router = express.Router();
 
 //  Register signup route
 router.post("/signup", SignupValidation, SignUp);
 
 router.post("/signin", SignInValidation, signIn);
+router.get("/jwt", JwtValiadtion, JwtLogin);
 
 export default router;
