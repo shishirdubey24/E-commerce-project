3ea54f894e25788136efe4643124a4f0a00ea912
Shishir Dubey <shishirdubey221@gmail.com>
2026-06-01 13:27:38 +0530

    "Changed response structure"

--- actual-backend/Controller/Auth/SignIn.js
+++ actual-backend/Controller/Auth/SignIn.js
@@ -23,8 +23,8 @@ export const signIn = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 60 * 60 * 1000,
     });
     res.status(200).json({
