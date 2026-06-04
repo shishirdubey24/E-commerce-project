08488c66017f97769d44c920fc33fa552974f7ff
Shishir Dubey <shishirdubey221@gmail.com>
2026-06-04 12:35:58 +0530

    "Updated imports"

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 import Footer from "../components/UI/Footer"
 import Header from "../components/pages/HomePage/Header";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { Suspense } from "react";
 function PageBodyLoader() {
   return (
