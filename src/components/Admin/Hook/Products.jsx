a61f654fe84529cea0a8242a606adda7c6d7b17b
Shishir Dubey <shishirdubey221@gmail.com>
2026-06-06 13:30:43 +0530

    "changed the route"

--- src/components/Admin/Hook/Products.jsx
+++ src/components/Admin/Hook/Products.jsx
@@ -1,7 +1,9 @@
 import { useEffect} from "react";
 import { useDispatch } from "react-redux";
+
 import { adminPanelActions } from "../../../store/adminSlice";
-const BASE_URL = "https://e-commerce-project-76em.onrender.com";
+import { API_BASE_URL } from "../../../config/Api";
+//const BASE_URL = "https://e-commerce-project-76em.onrender.com";
 const Products=()=>{
  const dispatch=useDispatch();
  
@@ -9,7 +11,7 @@ const Products=()=>{
         const fetching=async()=>{
 try{
     dispatch(adminPanelActions.setLoading(true))
-    const data =await fetch(`${BASE_URL}/admin/products`)
+    const data =await fetch(`${API_BASE_URL}/admin/products`)
    if (!data.ok) throw new Error(`HTTP error! status: ${data.status}`);  
     const res= await data.json();
       dispatch(adminPanelActions.setAdminProducts(res))
